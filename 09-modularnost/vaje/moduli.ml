8290b4303aa8a878cd5dbdccf7f6afd8aae11be7
Gašper Terglav <gasper.terglav@student.fmf.uni-lj.si>
2019-12-02 11:55:09 +0100

    naredil na pol na vajah

--- 09-modularnost/vaje/moduli.ml
+++ 09-modularnost/vaje/moduli.ml
@@ -53,6 +53,13 @@ module type NAT = sig
 
   val eq   : t -> t -> bool
   val zero : t
+  val one : t
+  val add : t -> t -> t
+  val multiply : t -> t -> t
+  val subtract : t -> t -> t
+
+  val to_int : t -> int
+  val of_int : int -> t
   (* Dodajte manjkajoče! *)
   (* val to_int : t -> int *)
   (* val of_int : int -> t *)
@@ -70,8 +77,17 @@ end
 module Nat_int : NAT = struct
 
   type t = int
-  let eq x y = failwith "later"
+
+  let eq x y = x = y
   let zero = 0
+  let one = 1
+  let add x y = x + y
+  let multiply x y = x * y
+  let subtract x y = if x - y > 0 then x - y else 0
+
+  let to_int x = x
+  let of_int x = x
+
   (* Dodajte manjkajoče! *)
 
 end
@@ -90,12 +106,52 @@ end
 
 module Nat_peano : NAT = struct
 
-  type t = unit (* To morate spremeniti! *)
-  let eq x y = failwith "later"
-  let zero = () (* To morate spremeniti! *)
+  type t = Z | S of t (*Zero and Sucessor(n)*)
+
+  let zero = Z (* To morate spremeniti! *)
+  let one = S Z
+  (*let rec S x =
+    match x with
+    | Z -> S Z
+    | S z -> S (S z)*)
+    
+  let rec eq x y = 
+    match x, y with
+    | Z, Z -> true
+    | Z, S z -> false
+    | S z, Z -> false
+    | S z, S w -> eq z w
+  
+  let rec add x y =
+    match x with
+    | Z -> y 
+    | S z -> if y = Z then S z else S (add (S z) y)
+
+  let rec multiply x y =
+    match x with
+    | Z -> Z
+    | S z -> if y = Z then Z else add y (multiply z y)
+
+  let rec subtract x y = 
+    match x, y with
+    | Z, Z -> Z
+    | Z, S z -> Z
+    | S z, Z -> S z
+    | S z, S w -> subtract z w
+
+  let rec to_int x = 
+    match x with
+    | Z -> 0
+    | S n -> 1 + (to_int n)  
+
+  let rec of_int x = if x <= 0 then Z else S (of_int (x-1))
   (* Dodajte manjkajoče! *)
 
 end
+
+
+let three = Nat_peano.of_int 3
+let seven = Nat_peano.of_int 7
 
 (*----------------------------------------------------------------------------*]
  V OCamlu lahko module podajamo kot argumente funkcij, z uporabo besede
@@ -118,7 +174,17 @@ end
  - : int = 4950
 [*----------------------------------------------------------------------------*)
 
-let sum_nat_100 (module Nat : NAT) = ()
+let sum_nat_100 (module Nat : NAT) = 
+  let hundred = Nat.of_int 100 in
+  let rec sum_x_100 x = 
+    if Nat.eq x hundred then 
+      hundred
+    else
+      (* x + sum_x_100 (x + 1) *)
+      Nat.add x (sum_x_100 (Nat.add x Nat.one)) 
+  in 
+  sum_x_100 Nat.zero |> Nat.to_int
+
 
 (*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*]
  Now we follow the fable told by John Reynolds in the introduction.
@@ -133,7 +199,13 @@ let sum_nat_100 (module Nat : NAT) = ()
 module type COMPLEX = sig
   type t
   val eq : t -> t -> bool
-  (* Dodajte manjkajoče! *)
+  val zero : t
+  val one : t
+  val i : t
+  val negacija : t -> t
+  val konjugacija : t -> t
+  val sestevanje : t -> t -> t
+  val mnozenje : t -> t -> t
 end
 
 (*----------------------------------------------------------------------------*]
@@ -145,11 +217,18 @@ module Cartesian : COMPLEX = struct
 
   type t = {re : float; im : float}
 
-  let eq x y = failwith "later"
+  let eq x y = if x.re = y.re then x.im = y.im else false
+  let zero = {re = 0.; im = 0.}
+  let one = {re = 1.; im = 0.}
+  let i = {re = 0.; im = 1.}
+  let negacija x = {re = (-. x.re); im = (-. x.im)}
+  let konjugacija x = {re = x.re; im = (-. x.im)}
+  let sestevanje x y = {re = x.re +. y.re; im = x.im +. y.im}
+  let mnozenje x y = {re = x.re *. y.re -. x.im *. y.im; im = x.re *. y.im +. x.im *. y.re}
   (* Dodajte manjkajoče! *)
 
 end
-
+;;
 (*----------------------------------------------------------------------------*]
  Sedaj napišite še polarno implementacijo kompleksnih števil, kjer ima vsako
  kompleksno število radij in kot (angl. magnitude in argument).
@@ -167,7 +246,13 @@ module Polar : COMPLEX = struct
   let rad_of_deg deg = (deg /. 180.) *. pi
   let deg_of_rad rad = (rad /. pi) *. 180.
 
-  let eq x y = failwith "later"
+  let eq x y = 
+    (x.magn = 0. && y.magn = 0.)
+    || (x.magn = y.magn && x.arg = y.arg)
+
+  let zero = {magn = 0.; arg = 0.}
+  let one = {magn = 1.; arg = 0.}
+  let i = {magn = 1.; arg = pi /. 2.}
   (* Dodajte manjkajoče! *)
 
 end
